fe250f71ba95ff57550601f7729e4ae987747abe
Signup Solution <signupsolution.reactnative@gmail.com>
2021-11-10 18:36:48 +0500

    Added my project

---
+++ nuxt.config.js
@@ -0,0 +1,94 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'provider-portal',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'ant-design-vue/dist/antd.less',
+    '~assets/variables/light.less',
+    'assets/scss/main.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/antd-ui'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+ 
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    babel: {
+      plugins: [
+        [
+          'import',
+          { libraryName: 'antd', libraryDirectory: 'lib', style: true },
+          'antd'
+        ]
+        // [
+        //   'import',
+        //   { libraryName: 'antd-mobile', libraryDirectory: 'lib' },
+        //   'antd-mobile',
+        // ],
+      ]
+    }
+    /*
+     ** You can extend webpack config here
+     */
+  },
+  extend(config, ctx) {}
+}
