ce1932839991fb821e942511a9ee36cb962dd98a
Syed714Ahsan <ahsangillani714@gmail.com>
2021-11-22 15:29:28 +0500

    Ahsan Changes

--- components/Pages/TelehealthScheduler/TelehealthScheduler.vue
+++ components/Pages/TelehealthScheduler/TelehealthScheduler.vue
@@ -1,12 +1,25 @@
 <!-- ************************** TEMPLATE  ************************* -->
 <template>
   <div class="pt-4">
-    <a-row :gutter="24">
-      <a-col :xs="24" :sm="24" :md="18" :lg="15" :xl="16">
+    <a-row :gutter="24" type="flex">
+      <a-col
+        :xs="{ span: 24, order: 2 }"
+        :sm="{ span: 24, order: 2 }"
+        :md="{ span: 15, order: 0 }"
+        :lg="{ span: 15, order: 0 }"
+        :xl="{ span: 16, order: 0 }"
+      >
         <h3 class="font-600 fs-extra-large px-2 pb-2">Telehealth Scheduler</h3>
         <TelehealthSchedulerForm />
       </a-col>
-      <a-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8" class="pt-4 mt-3">
+      <a-col
+        class="pt-4 mt-3"
+        :xs="{ span: 24, order: 1 }"
+        :sm="{ span: 24, order: 1 }"
+        :md="{ span: 9, order: 0 }"
+        :lg="{ span: 9, order: 0 }"
+        :xl="{ span: 8, order: 0 }"
+      >
         <PatientDetail />
       </a-col>
     </a-row>
